e0dc862456e4c16c90083c6ec607bc6bdd216585
Josh Van Auken <vanaukenjosh@gmail.com>
2022-04-18 18:50:54 -0500

    Remove <br>

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-function questionOne() {
-    var hiit = document.getElementById('hiit');
-    var strength = document.getElementById('strength');
-
-    //If "HIIT" is selected, question 2 needs to ask user about height and weight. 
-    hiit.onclick = document.getElementById('form2').style.display='block';
-    //If "Strength" is selected, question 2 needs to ask what muscle group(s) user wants to exercise.
-    strength.onclick = document.getElementById('form3').style.display='block';
-}
-
-window.onload = questionOne();
-
 //Limit number of muscle group selections to 2.
 function validateMuscleGroupSelections() {
     var checkboxes = document.getElementsByName("muscleGroup");
