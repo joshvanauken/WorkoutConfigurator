c65572f57300b42753f7c035e93ff2fde1e8786a
Josh Van Auken <vanaukenjosh@gmail.com>
2022-04-18 17:35:45 -0500

    Forms 2 and 3, revision 0

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-var form = document.getElementById('form');
-form.addEventListener('submit', questionOne);
+window.onload = questionOne () {
+    var hiit = document.getElementById('hiit');
+    var strength = document.getElementById('strength');
 
-function questionOne(event) {
-    
+    //If "HIIT" is selected, question 2 needs to ask user about height and weight. 
+    hiit.onclick = document.getElementById('form2').style.display='block';
+    //If "Strength" is selected, question 2 needs to ask what muscle group(s) user wants to exercise.
+    strength.onclick = document.getElementById('form3').style.display='block';
+}
+
+//Limit number of muscle group selections to 2.
+function validateMuscleGroupSelections() {
+    var checkboxes = document.getElementsByName("muscleGroup");
+    var numberOfCheckedItems = 0;
+    var muscleGroup1;
+    var muscleGroup2;
+    for(var i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked) {
+            muscleGroup1 = checkboxes[i].id;
+            numberOfCheckedItems++;
+        }
+        //When at 2, the unchecked checkboxes need to have attribute of "grayed out" so that they are no longer selectable. 
+        if(numberOfCheckedItems == 2) {
+            muscleGroup2 = checkboxes[i].id;
+            
+        }
+    }
 }
